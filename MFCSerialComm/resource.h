d457e795aa4ea989cb3950925e797b8733ded99e
lwj789_CodeBox <lwj798@naver.com>
2021-08-20 18:14:28 +0900

    2021.08.20 - 18:14
    [Add]
      1. 버전 이미지 추가

--- MFCSerialComm/resource.h
+++ MFCSerialComm/resource.h
@@ -8,8 +8,9 @@
 #define IDB_DEL_DATA                    141
 #define IDB_EXIT_APP                    142
 #define IDB_SAVE_DATA                   145
-#define IDB_BITMAP1                     146
 #define IDB_COPY_DATA                   146
+#define IDB_BITMAP1                     148
+#define IDB_VER_DATA                    148
 #define IDC_TOOL_BAR_RECT               1000
 #define IDC_SERIAL_PORT_LIST            1001
 #define IDC_SEARCH                      1002
@@ -18,7 +19,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        147
+#define _APS_NEXT_RESOURCE_VALUE        149
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
